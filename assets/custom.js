8ca7faaed7440828430f62fee9887a431fda472d
meanttobe25 <dreamjob1025@gmail.com>
2023-02-02 01:41:00 +0200

    make before easy-care

--- assets/custom.js
+++ assets/custom.js
@@ -1,17 +1,23 @@
 function changePromotion(){
-   setTimeout(() => {
-        if(document.querySelectorAll('.promotion-wrapper') && document.querySelector('#gf-products')){
-            document.querySelectorAll('.promotion-wrapper').forEach((element)=>{
-                document.querySelector('#gf-products').insertBefore(element, document.querySelector('#gf-products').children[element.dataset.index - 1]);
-                element.style.display = 'block';
-            })
-        }
-   }, 500);
-        
-}
-let promotionInterval = setInterval(function(){
-    if(document.querySelector('.product-grid-block')){
-        clearInterval(promotionInterval);
-        changePromotion();
-    }
-},100)
+    setTimeout(() => {
+         if(document.querySelectorAll('.promotion-wrapper') && document.querySelector('#gf-products')){
+             document.querySelectorAll('.promotion-wrapper').forEach((element)=>{
+                 document.querySelector('#gf-products').insertBefore(element, document.querySelector('#gf-products').children[element.dataset.index - 1]);
+                 element.style.display = 'block';
+             })
+         }
+    }, 500);
+ }
+ let promotionInterval = setInterval(function(){
+     if(document.querySelector('.product-grid-block')){
+         clearInterval(promotionInterval);
+         changePromotion();
+     }
+ },100)
+
+ $(document).ready(function(){
+    $('.custom-reviews .review-items').slick({
+        infinite: true,
+      slidesToShow: 3
+     })
+ })
